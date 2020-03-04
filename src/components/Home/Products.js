c15f63388901d76800a40e191f993a5ef4811cd1
htrix <htrixe@gmail.com>
2020-03-04 13:21:36 +0000

    Poduct removed tracedSVG

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -14,7 +14,7 @@ const getProducts = graphql`
           image {
             fluid(maxHeight: 226) {
               src
-              ...GatsbyContentfulFluid_tracedSVG
+              ...GatsbyContentfulFluid
             }
           }
         }
